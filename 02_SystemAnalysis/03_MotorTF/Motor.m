c12ce6b1c7cf075fef46dfd7fd7c5ff0182d878e
fil-bad <filibad25051997@gmail.com>
2019-12-28 12:27:16 +0100

    Motor script updated after videocall.

--- 02_SystemAnalysis/03_MotorTF/Motor.m
+++ 02_SystemAnalysis/03_MotorTF/Motor.m
@@ -1,26 +1,35 @@
-%#####################################################################
+% ####################################################################
 % Analisi funzione di trasferimento motore progetto
 % 
 % La stima attuale è fatta con il motore a vuoto, si dovrà aggiungere
 % successivamente la massa e il disco con inerzia
 % 
-% 
+% Filippo Badalamenti
+% ####################################################################
 
 %%
 clc 
 clear variables
+format shortEng
 
 %% Dati con campionamento 10ms
 filename = 'test1.dat'; delimiterIn = '\t';
 Samples = importdata(filename,delimiterIn); 
 % matrice Nx4, con N misure effettuate
-%%
-Ts = 0.01; % a sample every 10ms
+
+%% 10ms sampling & Normalization
+
+timeWanted = 0.01; % time sampling in seconds (-> 10ms)
+
+Ts = floor(16000*1000*timeWanted/1024)*0.064*0.001;
 Fs = 1/Ts;
+step2rad = 11/(2*pi); % STEPs/rad
 
-maxSpeed = mean(Samples.data(5676:5926,3))/Ts; % STEPs/second
+Samples.data(:,3) = Samples.data(:,3)/(Ts*step2rad); % rad/s
+Samples.data(:,1) = Samples.data(:,1)/255; % PWM in percentage
 
-Samples.data(:,3) = Samples.data(:,3)/Ts;
+maxSpeed_10 = mean(Samples.data(5676:5926,3));
+
 
 
 
@@ -53,7 +62,7 @@ input_1(1:4) = 0;
 %%
 datas = iddata(Set2_1, input_1, Ts);
 
-Sys = tfest(datas, 1)
+Sys_c = tfest(datas, 1)
 
 Sys_d = tfest(datas, 1, 'Ts', Ts)
 
@@ -63,33 +72,42 @@ Sys_c2 = d2c(Sys_d,'tustin');
 
 %%
 figure(2)
-step(Sys, 'k', Sys_c1,'r', Sys_c2,'b');
+step(Sys_c, 'k', Sys_c1,'r', Sys_c2,'b');
 legend('Sys_direct','Sys_{zoh}','Sys_{tustin}')
 
 
 
 
 
-%%
+%% New Analysis
 clc
 clear variables
+
 
 %% Dati con campionamento 2ms
 
 filename = 'test0_255.dat'; delimiterIn = '\t';
-Samples = importdata(filename,delimiterIn); 
+Samples = importdata(filename,delimiterIn);
 % matrice Nx4, con N misure effettuate
-%%
-Ts = 0.002; % a sample every 2ms
+
+%% Normalization
+
+timeWanted = 0.002; % time sampling in seconds (-> 2ms)
+
+Ts = floor(16000*1000*timeWanted/1024)*0.064*0.001;
 Fs = 1/Ts;
+step2rad = 11/(2*pi); % STEPs/rad
 
-maxSpeed = mean(Samples.data(190:1190,3)); % STEPs/second
+Samples.data(:,1) = Samples.data(:,1)/255; % PWM in percentage
+Samples.data(:,3) = Samples.data(:,3)/(Ts*step2rad); % rad/s
 
-% Samples.data(:,3) = Samples.data(:,3)/Ts;
+maxSpeed_2 = mean(Samples.data(190:1190,3)); 
 
 
 
-%% Analizzo un 0-255
+
+
+%% Analizzo diversi 0-255
 
 Set_1 = Samples.data(5:255,2);
 input_1 = Samples.data(5:255,1);
@@ -113,14 +131,16 @@ data_4 = iddata(Set_4, input_4, Ts);
 
 
 datas = merge(data_1, data_2, data_3, data_4);
-Sys = tfest(datas, 2, 0)
-Sys.Denominator(3) = 0;
+Sys_c = tfest(datas, 2, 0)
+Sys_c.Denominator(3) = 0;
 Sys_d = tfest(datas, 2, 0, 'Ts', Ts);
 
 %%
+tMax = 10; % we stop simulation after 10 seconds
 figure(2)
-step(Sys_d,'b', Sys, 'r');
+step(Sys_d,'b', Sys_c, 'r', tMax);
 legend('Sys_{disc}','Sys_{cont}') 
+ylabel('Steps')
 % c'è nella simulazione del continuo, un polo vicino allo zero ma NON zero,
 % per cui otteniamo un sistema del secondo ordine ma convergente, se lo
 % sottoponiamo ad uno step; se pongo Sys.Denominator = 0 in maniera
@@ -132,12 +152,21 @@ legend('Sys_{disc}','Sys_{cont}')
 figure(3)
 rlocus(Sys_d)
 figure(4)
-rlocus(Sys)
+rlocus(Sys_c)
+
+% analisi fatta con la simulazione dello step; ho preso a 5 e 6 secondi i
+% valori del sys_c e del sys_d, fatta la differenza degli estremi e
+% confrontata con il numero di step che dovrebbe fare in un sec (6820 ca).
+% Per sys_c a regime se ne hanno 7300ca, per sys_d 7100ca, con un errore a
+% regime percentuale rispettivamente del 6.58% e 3.94%
+% TODO: capire come tirare fuori i parametri dal discreto.
 
 %% Spazio di stato
 
 [Ad, Bd, Cd, Dd, ts] = ssdata(Sys_d);
 
+
+%%
 
 
 
