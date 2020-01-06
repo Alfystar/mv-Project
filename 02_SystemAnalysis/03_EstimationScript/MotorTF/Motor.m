e9c8229a90104a1089a594970c5ad049effa22a6
fil-bad <filibad25051997@gmail.com>
2020-01-06 21:18:52 +0100

    Added tf estimation with speed.

--- 02_SystemAnalysis/03_EstimationScript/MotorTF/Motor.m
+++ 02_SystemAnalysis/03_EstimationScript/MotorTF/Motor.m
@@ -42,7 +42,7 @@ maxSpeeds_xMs(6) = mean(test_12ms.data(35:230,3))/ times_xs(6);
 maxSpeeds_xMs(7) = mean(test_14ms.data(30:160,3))/ times_xs(7);
 maxSpeeds_xMs(8) = mean(test_15ms.data(30:150,3))/ times_xs(8);
 
-figure(10)
+figure(1)
 plot(times_xs, maxSpeeds_xMs, 'r-.', 'Marker', '*','MarkerEdgeColor','b')
 xlabel('Sample time (s)'); ylabel('Not normalized speed')
 legend('Speed_{max}')
@@ -89,7 +89,7 @@ P1 = P2(1:len/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(len/2))/len;
 
-figure(1)
+figure(2)
 plot(f,P1,'b')
 legend('P1')
 title('Single-Sided Amplitude Spectrum of Set_1(t)')
@@ -98,10 +98,10 @@ ylabel('|P1(f)|')
 
 %% Preparing sets
 % We have a Nx2 matrix 
-Set_1 = Samples_10ms(10:190,1:2); 
-Set_2 = Samples_10ms(365:695,1:2);
-Set_3 = Samples_10ms(870:1140,1:2); 
-Set_4 = Samples_10ms(1200:1480,1:2);
+Set_1 = Samples_10ms(10:190,1:3); 
+Set_2 = Samples_10ms(365:695,1:3);
+Set_3 = Samples_10ms(870:1140,1:3); 
+Set_4 = Samples_10ms(1200:1480,1:3);
 Set_4(1:34,1) = 0; %% prendiamo dei dati da una frenata forzata
 
 %% Merging datas
@@ -154,46 +154,56 @@ ylabel('Steps')
 % regime percentuale rispettivamente del 0.11% per entrambi
 % TODO: capire come tirare fuori i parametri dal discreto.
 
+%% Soft-braking transfer function
 
-%% Hard-braking transfer function
+Set_s = Samples_10ms(1045:1180,1:3);
+Set_s(106:end,1) = 0;
 
-Set_h1 = Samples_10ms(1045:1180,1:2);
-Set_h1(106:end,1) = 0;
-Set_h2 = Samples_10ms(1385:1520,1:2);
-Set_h2(106:end,1) = 0;
+data_s = iddata(Set_s(:,2), Set_s(:,1), Ts);
 
-data_h1 = iddata(Set_h1(:,2), Set_h1(:,1), Ts);
-data_h2 = iddata(Set_h2(:,2), Set_h2(:,1), Ts);
-datas_h = merge(data_h1, data_h2);
-
-Sys_hC = tfest(datas_h, 2, 0); % ?? why is not working??
-Sys_hD = tfest(datas_h, 2, 0, 'Ts', Ts) %% this is ok, maybe?
+Sys_sC = tfest(data_s, 2, 0); % ?? why is not working??
+Sys_sD = tfest(data_s, 2, 0, 'Ts', Ts) %% this is ok, maybe?
 
 %%
 figure(5)
-rlocus(Sys_hD)
+rlocus(Sys_sC)
 figure(6)
-rlocus(Sys_hC)
-
-%% Comparing with soft-braking
-
-Set_s1 = Samples_10ms(175:365,1:2);
-Set_s2 = Samples_10ms(675:865,1:2);
-
-data_s1 = iddata(Set_s1(:,2), Set_s1(:,1), Ts);
-data_s2 = iddata(Set_s2(:,2), Set_s2(:,1), Ts);
-datas_s = merge(data_s1, data_s2);
-
-Sys_sC = tfest(datas_s, 2, 0); % ?? why is not working??
-Sys_sD = tfest(datas_s, 2, 0, 'Ts', Ts) %% this is ok, maybe?
+rlocus(Sys_sD)
 
 
+%% Hard-braking transfer function
+
+Set_h = Samples_10ms(1385:1520,1:3);
+Set_h(106:end,1) = 0;
+
+data_h = iddata(Set_h(:,2), Set_h(:,1), Ts);
+
+Sys_hC = tfest(data_h, 2, 0); % ?? why is not working??
+Sys_hD = tfest(data_h, 2, 0, 'Ts', Ts) %% this is ok, maybe?
 
 %%
 figure(7)
-rlocus(Sys_sD)
+rlocus(Sys_hC)
 figure(8)
-rlocus(Sys_sC)
+rlocus(Sys_hD)
+
+%% Comparing with free-releasing
+
+Set_f1 = Samples_10ms(170:365,1:3);
+Set_f2 = Samples_10ms(675:865,1:3);
+
+data_f1 = iddata(Set_f1(:,2), Set_f1(:,1), Ts);
+data_f2 = iddata(Set_f2(:,2), Set_f2(:,1), Ts);
+datas_f = merge(data_f1, data_f2);
+
+Sys_fC = tfest(datas_f, 2, 0); % ?? why is not working??
+Sys_fD = tfest(datas_f, 2, 0, 'Ts', Ts) %% this is ok, maybe?
+
+%%
+figure(9)
+rlocus(Sys_fD)
+figure(10)
+rlocus(Sys_fC)
 
 %% rlocus and parameters with d2c
 % considerazioni: il polo molto piccolo può essere cancellato come nel caso
@@ -202,13 +212,73 @@ rlocus(Sys_sC)
 % gain per s=0 non è altro che il ka desiderato (o si pone s = 0, o si
 % vede il valore sempre in getpar).
 
-Sys_hD_d2c = d2c(Sys_hD)
-getpar(Sys_hD_d2c) 
-
 Sys_sD_d2c = d2c(Sys_sD)
 getpar(Sys_sD_d2c) 
 
+Sys_hD_d2c = d2c(Sys_hD)
+getpar(Sys_hD_d2c) 
+
+Sys_fD_d2c = d2c(Sys_fD)
+getpar(Sys_fD_d2c) 
+
+
 figure(17)
-rlocus(Sys_hD_d2c)
-figure(18)
 rlocus(Sys_sD_d2c)
+figure(18)
+rlocus(Sys_hD_d2c)
+figure(19)
+rlocus(Sys_fD_d2c)
+
+%% New braking analysis 
+% We have three different situations:
+% 1) free release (from 255 to 0)
+% 2) soft braking (from 255 to 999)
+% 3) hard braking (from -255 to -999)
+% Now we are going to find the rho parameter, using only speed (not steps)
+
+data_f1V = iddata(Set_f1(:,3), Set_f1(:,1), Ts);
+data_f2V = iddata(Set_f2(:,3), Set_f2(:,1), Ts);
+
+datas_fV = merge(data_f1V, data_f2V);
+
+data_sV = iddata(Set_s(:,3), Set_s(:,1), Ts);
+data_hV = iddata(Set_h(:,3), Set_h(:,1), Ts);
+
+Sys_fVC = tfest(datas_fV, 1, 0)%, 'Ts', Ts) %% this is ok, maybe?
+Sys_sVC = tfest(data_sV, 1, 0)%, 'Ts', Ts) %% this is ok, maybe?
+Sys_hVC = tfest(data_hV, 1, 0)%, 'Ts', Ts) %% this is ok, maybe?
+
+% comparing with accelerating system
+data_sysV1 = iddata(Set_1(:,3), Set_1(:,1), Ts);
+data_sysV2 = iddata(Set_2(:,3), Set_2(:,1), Ts);
+data_sysV3 = iddata(Set_3(:,3), Set_3(:,1), Ts);
+data_sysV4 = iddata(Set_4(:,3), Set_4(:,1), Ts);
+
+datas_sysV = merge(data_sysV1, data_sysV2, data_sysV3, data_sysV4);
+
+Sys_sysVC = tfest(datas_sysV, 1, 0)%, 'Ts', Ts) %% this is ok, maybe?
+
+% con questo metodo evitiamo di far stimare anche il polo che sappiamo
+% essere in zero (e che tfest potrà trovare solo con un numero enorme di
+% campioni), così riusciamo ad estrapolare solo il polo d'interesse (rho)
+% e la costante ka del sistema
+
+%%
+figure(50)
+h = bodeplot(Sys_sysVC);
+showConfidence(h,3)
+%%
+
+sys_sim = ssest(data_sV, 1, 'Ts', Ts)
+tempo = 0:Ts:length(Set_s(106:end,1))*Ts';
+figure(51)
+Simul = lsim(sys_sim, zeros(1,length(tempo)), tempo, Set_s(106,3))/sys_sim.C;
+% hold on
+% plot(tempo, Set_s(105:end,3),'r')
+
+
+
+
+
+
+
