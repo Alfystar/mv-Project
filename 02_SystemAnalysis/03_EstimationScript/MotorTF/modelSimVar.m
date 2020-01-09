82c388c29aa0ff66a313dadb8aa56e6471602549
alfystar <alfystar1701@gmail.com>
2020-01-09 10:23:27 +0100

    estimation motor automation and update simulation

--- 02_SystemAnalysis/03_EstimationScript/MotorTF/modelSimVar.m
+++ 02_SystemAnalysis/03_EstimationScript/MotorTF/modelSimVar.m
@@ -79,9 +79,9 @@ brakingFrame = Samples_10ms(bfStart:bfEnd,1:3);
 % Ottenimento attDyn e rhoMot
 t=0:Ts:(length(freeRelaseSample)-1)*Ts;
 [fitresult, gof] = freeRelaseEst(t, freeRelaseSample);
-attDyn = abs(fitresult.uDym)
-rhoAtt = abs(fitresult.rhoMec)
-velmaxFit = abs(fitresult.vMax)
+uDym = abs(fitresult.uDym);
+rhoMec = abs(fitresult.rhoMec);
+velmaxFit = abs(fitresult.vMax);
 
 % Ottenimento rhoInd+(attDyn+rhoMot) da accelerazione e frenata soft
 Set_fin_b = Samples_10ms(850:1220,1:3);
@@ -95,55 +95,25 @@ brakingFrameData = iddata(brakingFrame(:,3), brakingFrame(:,1), Ts);
 
 tfMot_b = tfest(brakingFrameData, 1); % 'Ts',Ts
 
-ka = tfMot_b.Numerator
-rho = tfMot_b.Denominator(2)    % Rho tot (rhoInd + rho_att)
-velmax = tfMot_b.Numerator/tfMot_b.Denominator(2)
-% 
-% 
-% % Ottenimento attDyn e rhoMot
-% % free release (from 255 to 0)
-% Set_f1 = Samples_10ms(170:365,1:3);
-% Set_f2 = Samples_10ms(675:865,1:3);
-% 
-% rhoAttDyn1 = (Set_f1(176,3)-Set_f1(30,3))/((176-30)*Ts);
-% rhoAttDyn2 = (Set_f1(167,3)-Set_f1(23,3))/((167-23)*Ts);
-% rhoAttDyn = (rhoAttDyn1+rhoAttDyn2)/2;
-% rhoAttDyn = abs(rhoAttDyn)
-% 
-% % Cerco di capire l'attrito vischioso meccanico del motore
-% onlyDecrease = Samples_10ms(170+30:170+176,1:3);
-% d=onlyDecrease(:,3);
-% % for k=1:length(onlyDecrease)
-% %   onlyDecrease(k,3) = onlyDecrease(k,3) - rhoAttDyn + rhoAttDyn*k*Ts ;
-% % end
-% % figure(1)
-% % clf
-% % t=0:Ts:(length(onlyDecrease)-1)*Ts;
-% % plot(t(:),onlyDecrease(:,3));
-% % yl = ylim; % Get current limits.
-% % ylim([0, yl(2)]); 
-% t=0:Ts:(length(onlyDecrease)-1)*Ts;
-% var=fit(t(:),d,'exp2')
-% 
-% 
-% data_f1V = iddata(Set_f1(:,3), Set_f1(:,1), Ts);
-% data_f2V = iddata(Set_f2(:,3), Set_f2(:,1), Ts);
-% datas_fV = merge(data_f1V, data_f2V);
-% 
-% Sys_fVC = tfest(datas_fV, 1);%, 'Ts', Ts) %% this is ok, maybe?
-% 
-% rhoAtt = Sys_fVC.Denominator(2) 
-
+ka = tfMot_b.Numerator;
+rho = tfMot_b.Denominator(2);    % Rho tot (rhoInd + rho_att)
+velmax = tfMot_b.Numerator/tfMot_b.Denominator(2);
 
 % Ottenimento rhoMag
-rhoInd = rho - rhoAtt
+rhoInd = rho - rhoMec;
 
+% Printo valori notevoli
+ka
+rhoInd 
+rhoMec
+uDym
 
+fprintf("Tra il fitting e la stima ecco le vel max stimate:\n")
+fprintf("velmaxFit=%f\tvelmax=%f\tdist=%f\n", velmaxFit, velmax, abs(velmaxFit-velmax));
 
 % Condizioni a contorno
-v0 =0;
-
+v0 = 0;
 
 % vettori di test
-setU_Norm = Samples_10ms(1:850,1);
-setV_Norm = Samples_10ms(1:850,3);
+setU_Norm = Samples_10ms(1:bfStart,1);
+setV_Norm = Samples_10ms(1:bfStart,3);
