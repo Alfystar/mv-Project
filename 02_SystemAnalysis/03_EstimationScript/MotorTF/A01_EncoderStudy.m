4bb9f1837693f4e4a3fbef5b95d7ac9a89df2452
alfystar <alfystar1701@gmail.com>
2020-02-15 16:47:58 +0100

    fft encoder

--- 02_SystemAnalysis/03_EstimationScript/MotorTF/A01_EncoderStudy.m
+++ 02_SystemAnalysis/03_EstimationScript/MotorTF/A01_EncoderStudy.m
@@ -64,20 +64,56 @@ legend('Test_{2ms}','Test_{4ms}','Test_{6ms}','Test_{8ms}',...
 
 figure(3)
 
-maxSpeeds_xMs = zeros(1,7);
+% maxSpeeds_xMs = cell(7,1);
+maxSpeedsMean_xMs = zeros(1,7);
 times_xs = [en_2ms.Time(2) en_4ms.Time(2) en_6ms.Time(2) en_8ms.Time(2) en_10ms.Time(2) en_12ms.Time(2) en_14ms.Time(2)];
 
+maxSpeeds_xMs(1,:) = {en_2ms.mVel(159:553)/ en_2ms.Time(2)*step2rad};
+maxSpeeds_xMs(2,:) = {(en_4ms.mVel(49:288)/ en_4ms.Time(2)*step2rad)};
+maxSpeeds_xMs(3,:) = {en_6ms.mVel(38:197)/ en_6ms.Time(2)*step2rad};
+maxSpeeds_xMs(4,:) = {en_8ms.mVel(30:152)/ en_8ms.Time(2)*step2rad};
+maxSpeeds_xMs(5,:) = {en_10ms.mVel(31:114)/ en_10ms.Time(2)*step2rad};
+maxSpeeds_xMs(6,:) = {en_12ms.mVel(27:115)/ en_12ms.Time(2)*step2rad};
+maxSpeeds_xMs(7,:) = {en_14ms.mVel(22:107)/ en_14ms.Time(2)*step2rad};
 
-maxSpeeds_xMs(1) = mean(en_2ms.mVel(159:553))/ en_2ms.Time(2)*step2rad;
-maxSpeeds_xMs(2) = mean(en_4ms.mVel(49:288))/ en_4ms.Time(2)*step2rad;
-maxSpeeds_xMs(3) = mean(en_6ms.mVel(38:197))/ en_6ms.Time(2)*step2rad;
-maxSpeeds_xMs(4) = mean(en_8ms.mVel(30:152))/ en_8ms.Time(2)*step2rad;
-maxSpeeds_xMs(5) = mean(en_10ms.mVel(31:114))/ en_10ms.Time(2)*step2rad;
-maxSpeeds_xMs(6) = mean(en_12ms.mVel(27:115))/ en_12ms.Time(2)*step2rad;
-maxSpeeds_xMs(7) = mean(en_14ms.mVel(22:107))/ en_14ms.Time(2)*step2rad;
+for k=1:7
+    maxSpeedsMean_xMs(k) = mean(maxSpeeds_xMs{k});
+end
 
 figure(3)
-plot(times_xs, maxSpeeds_xMs, 'r', 'Marker', '*','MarkerEdgeColor','b')
+clf
+plot(times_xs, maxSpeedsMean_xMs, 'r', 'Marker', '*','MarkerEdgeColor','b')
 grid on
 xlabel('Sample time (s)'); ylabel('Rad/sec mean')
 legend('Speed_{max}')
+
+figure(4)
+clf
+fftMaxSpeed = maxSpeeds_xMs;
+
+for k=1:7
+    fft_Test_f = fft(fftMaxSpeed{k}-maxSpeedsMean_xMs(k),200);
+    
+    Fs = 1/times_xs(k);
+    len = length(fft_Test_f);
+    
+    P2 = abs(fft_Test_f/len);
+    P1 = P2(1:len/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(len/2))/len;
+    
+    plot(f,P1)
+    grid on
+    hold on
+end
+% fft_Test_f = fft(maxSpeedsMean_xMs(1));
+
+% 
+% plot(f,P1,'b')
+% grid on
+% legend('V(f)')
+legend('Test_{2ms}','Test_{4ms}','Test_{6ms}','Test_{8ms}',...
+    'Test_{10ms}','Test_{12ms}','Test_{14ms}')
+title('Encoder rumor at step input')
+xlabel('f(Hz)')
+ylabel('|V(f)|')
